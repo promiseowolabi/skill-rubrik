d3d3590a1e40b969ac11bb5a5489eb87bf0ac080
promiseowolabi <promise.owolabi@me.com>
2019-07-21 22:10:48 +0100

    added a sql unmount function

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ class RubrikSkill(Skill):
         rubrik.vsphere_live_unmount(mounted_vm_name)
         await message.respond('All done! {} has been unmounted'.format(mounted_vm_name))
 
-    @match_regex('live mount db (?P<db_name>[\w\'-]+) from date (?P<date>[\w\'-]+) on time (?P<time>[0-9:]+\s[AMP]+) as a clone called (?P<mount_name>[\w-]+) instance (?P<sql_instance>[\w-]+) host (?P<sql_host>.+)')
+    @match_regex('live mount db (?P<db_name>[\w\'-]+) from date (?P<date>[\w\'-]+) on time (?P<time>[0-9:]+\s[AMP]+) as a clone called (?P<mount_name>[\w-]+) on instance (?P<sql_instance>[\w-]+) host (?P<sql_host>.+)')
     async def sqllivemount(self, message):
         db_name = message.regex.group('db_name')
         date = message.regex.group('date')
@@ -35,9 +35,12 @@ class RubrikSkill(Skill):
         rubrik.sql_live_mount(db_name, date, time, sql_instance, sql_host, mount_name)
         await message.respond('All done! {} has been live mounted as {}'.format(db_name, mount_name))
 
-    @match_regex('unmount db (?P<mounted_db_name>[\w\'-:\s]+)')
+    @match_regex('unmount db (?P<mounted_db_name>[\w\'-:\s]+) on instance (?P<sql_instance>[\w-]+) host (?P<sql_host>.+)')
     async def sqlliveunmount(self, message):
         mounted_db_name = message.regex.group('mounted_db_name')
+        sql_instance = message.regex.group('sql_instance')
+        sql_host = message.regex.group('sql_host')
+        sql_host = sql_host[4:-4]
         rubrik = rubrik_cdm.Connect()
-        rubrik.sql_live_unmount(mounted_db_name)
+        rubrik.sql_live_unmount(mounted_db_name, sql_instance, sql_host)
         await message.respond('All done! {} has been unmounted'.format(mounted_db_name))
